e31860468679525e3ea6e93b43844cba30dcd488
lhj963 <2442738006@qq.com>
2021-08-19 11:10:11 +0800

    编写0.1版本的makefile

---
+++ first/demo.h
@@ -0,0 +1,8 @@
+#ifndef _INCLUDE_DEMO_H_
+#define _INCLUDE_DEMO_H_
+
+void demo();
+
+
+
+#endif
---
+++ first/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "demo.h"
+#include "test.h"
+
+
+int main(){
+    demo();
+    test();
+    std::cout << "hello,i'am main."<<std::endl;
+}
